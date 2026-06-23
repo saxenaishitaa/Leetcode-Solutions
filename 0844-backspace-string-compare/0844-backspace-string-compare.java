b32810a090d69b50f48b51f5c0eb8ae4db259fb6
saxenaishitaa <saxena.ishitaa@gmail.com>
2026-06-23 10:40:45 +0530

    Time: 3 ms (21.06%), Space: 43.1 MB (50.39%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,51 +1,19 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i = s.length() - 1;
-        int j = t.length() - 1;
+        return buildString(s).equals(buildString(t));
+    }
+    
+    private String buildString(String str) {
+        Stack<Character> stack = new Stack<>();
         
-        int sSkip = 0;
-        int tSkip = 0; 
-        
-        while (i >= 0 || j >= 0) {
-           
-            while (i >= 0) {
-                if (s.charAt(i) == '#') {
-                    sSkip++;
-                    i--;
-                } else if (sSkip > 0) {
-                    sSkip--;
-                    i--;
-                } else {
-                    break;
-                }
+        for (char c : str.toCharArray()) {
+            if (c != '#') {
+                stack.push(c); 
+            } else if (!stack.isEmpty()) {
+                stack.pop(); 
             }
-            while (j >= 0) {
-                if (t.charAt(j) == '#') {
-                    tSkip++;
-                    j--;
-                } else if (tSkip > 0) {
-                    tSkip--;
-                    j--;
-                } else {
-                    break;
-                }
-            }
-            
-           
-            if (i >= 0 && j >= 0) {
-                if (s.charAt(i) != t.charAt(j)) {
-                    return false;
-                }
-            } 
-           
-            else if ((i >= 0) != (j >= 0)) {
-                return false;
-            }
-            
-            i--;
-            j--;
         }
-        
-        return true;
+       
+        return String.valueOf(stack);
     }
 }
